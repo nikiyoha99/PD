1e5518825b36861cbc44c768b8a0bd375eb276bd
nikiyoha99 <nikita20091912@gmail.com>
2025-10-02 11:36:22 +0300

    Add files via upload
    8. Kas ir GitHub un kāpēc to izmanto programmētāji?
    GitHub ir tiešsaistes platforma koda glabāšanai, versiju kontrolei un sadarbībai. Programmētāji to izmanto, lai efektīvi pārvaldītu kodu, strādātu komandā un izsekotu izmaiņām projektā.
    
    9. Kāpēc projektam var būt svarīgi izvēlēties licenci GitHub?
    Licence nosaka, kā citi drīkst izmantot, koplietot un mainīt projekta kodu, aizsargājot autoru tiesības un nodrošinot skaidrību lietotājiem.

---
+++ Untitled-1.py
@@ -0,0 +1,37 @@
+vards= "Nikita"
+vecums = 15 
+print("Mans vards ir" , vards,"un man ir","gadi") 
+
+a = 5
+b = 7 
+print("Summa ir:", a+b)
+
+sk= 9 
+if sk % 2 == 0:
+print("Pāra")
+else:
+print("nepāra")
+
+x = -3
+ if x > 0:
+   print("Pozītivs")
+ elif x >0:
+  print("Negatīvs")
+ else:
+ print("Nule")
+
+age = 15
+if age >=18
+print("Pilngadīgs")
+else:
+print("Nepilngadīgs")
+
+num = 15
+if num >= 10 and num <=20:
+ print("Skaitlis ir starp 10 un 20")
+else:
+ print("Nav šaja intervalā")
+
+
+ for i in range(1,11):
+  print(i)
